a55e65932e6013404572425736e55ba4b9089277
devmashum <dev.mashum@gmail.com>
2023-09-19 18:15:13 +0200

    final commit

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,11 +1,13 @@
 import axios, { Axios } from "axios";
 import { useEffect, useState } from "react";
 import React, { PureComponent } from 'react';
+import { Dna } from "react-loader-spinner";
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 
 const Phones = () => {
     const [phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -18,12 +20,20 @@ const Phones = () => {
                     }
                     return obj;
                 })
-                setPhones(phonesWithFakeData)
+
+                setPhones(phonesWithFakeData);
+                setLoading(false);
             })
     }, [])
 
     return (
         <div>
+            {loading && <Dna visible={true}
+                height="80"
+                width="80"
+                ariaLabel="dna-loading"
+                wrapperStyle={{}}
+                wrapperClass="dna-wrapper" />}
             <h2>Phones: {phones.length}</h2>
             <BarChart width={1300} height={400} data={phones}>
                 <Bar dataKey="price" fill="blue" />
